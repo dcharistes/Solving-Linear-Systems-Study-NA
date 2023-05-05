254fa4b2dacc3d1b9caa3d9592ef83b70bc08951
Dimitrios Bismpas <121557787+Dimbsb@users.noreply.github.com>
2023-05-05 18:42:19 +0300

    Add files via upload

--- untitled.m
+++ untitled.m
@@ -21,8 +21,8 @@ for i = 1:size(N, 2)
     avgP(i) = 0;
 end
 
-for n=4:35
-    m=n-3;
+for n = 4:35
+m = n-3;
 for i=1:10
 
 
@@ -33,34 +33,33 @@ A = round(10*rand(1,n-1))+1;
 B = round(10*rand(1,n-2))+1;
 
 p=pentadiagonal(E,C,D,A,B);
-
 y = round(100*rand(1,n)) + 1;
-
 b = y';
 
 tic;xM=(p\b)'; tM=toc;
 sumM(m)= sumM(m)+tM;
 
-tic;xP=PTRANSII(n,E,C,D,A,B,y); tP=toc;
-sumP(m)= sumP(m)+tP;
-
 tic;xG= gaussianElimination(p,b)'; tG=toc;
 sumG(m)= sumG(m)+tG;
+
+tic;xP=PTRANSII(n,E,C,D,A,B,y); tP=toc;
+sumP(m)= sumP(m)+tP;
 
 tic;xC=cramer(p,b)'; tC=toc;
 sumC(m) = sumC(m)+tC;
 
 end
 
-avgM(m)=sumM(m)/i;
-avgP(m)=sumP(m)/i;
 avgC(m)=sumC(m)/i;
 avgG(m)=sumG(m)/i;
+avgM(m)=sumM(m)/i;
+avgP(m)=sumP(m)/i;
+
 
 end
 
 subplot(2,1,1);
-plot(N,avgG,'g',N,avgC,'C',N,avgM,'r',N,avgP,'b');hold on; grid on;
+plot(N,avgG,'g--',N,avgC,'Co-',N,avgM,'r*-',N,avgP,'b^-');hold on; grid on;
 axis([0,35,0,2*(10^-3)]);
 hold off;
 
@@ -77,8 +76,11 @@ for i = 1:size(N, 2)
     avgP(i) = 0;
 end
 
-for n=4:50:2000
-    m=((n-4)/50)+1;
+n = 4;
+m = 1;
+while n <= 2000
+    m = ((n-4)/50) + 1;
+    n = n + 50;
 for i=1:10
 
 E = round(10*rand(1,n-2))+1;
@@ -87,17 +89,14 @@ D = round(10*rand(1,n))+1;
 A = round(10*rand(1,n-1))+1;
 B = round(10*rand(1,n-2))+1;
 p=pentadiagonal(E,C,D,A,B);
-
 y = round(100*rand(1,n)) + 1;
-
 b=y';
-
-tic;xM=(p\b)'; tM=toc;
-sumM(m)= sumM(m)+tM;
 
 tic;xP=PTRANSII(n,E,C,D,A,B,y); tP=toc;
 sumP(m)= sumP(m)+tP;
 
+tic;xM=(p\b)'; tM=toc;
+sumM(m)= sumM(m)+tM;
 end
 
 avgM(m)=sumM(m)/i;
@@ -106,7 +105,7 @@ avgP(m)=sumP(m)/i;
 end
 
 subplot(2,1,2);
-plot(N, avgM, 'r',N,avgP, 'b'); hold on; grid on;
+plot(N, avgM, 'r*-',N,avgP, 'bo-'); hold on; grid on;
 axis([0,2000,0,0.08]);
 hold off;
 
@@ -130,55 +129,45 @@ d = D;
 a = [A A(n)];
 b = [B B(n-1) B(n)];
 
+psi(n) = d(n);
+    sigma(n) = c(n) / psi(n);
+    fi(n) = e(n) / psi(n);
+    w(n) = y(n) / psi(n);
 
-psi(n)=d(n);
-s(n)=c(n)/psi(n);
-f(n)=e(n)/psi(n);
-w(n)=y(n)/psi(n);
-r(n-1)=a(n-1);
-psi(n-1) = d(n-1) - (s(n)*r(n-1));
-s(n-1) = (c(n-1)- (f(n)*r(n-1)))/psi(n-1);
-f(n-1) = e(n-1)/psi(n-1);
-w(n-1) = (y(n-1) - (w(n)*r(n-1)))/psi(n-1);
+    ro(n-1) = a(n-1);
+    psi(n-1) = d(n-1) - sigma(n) * ro(n-1);
+    sigma(n-1) = (c(n-1) - fi(n) * ro(n-1)) / psi(n-1);
+    fi(n-1) = e(n-1) / psi(n-1);
+    w(n-1) = (y(n-1) - w(n) * ro(n-1)) / psi(n-1);
 
-for i=n-2:-1:3
-r(i)=a(i)-(s(i+2)*b(i));
-psi(i)=d(i) - (f(i+2)*b(i)) - (s(i+1)*r(i));
-s(i)=(c(i)-(f(i+1)*r(i)))/psi(i);
-f(i)=e(i)/psi(i);
-w(i)=(y(i)-(w(i+2)*b(i))-(w(i+1)*r(i)))/psi(i);
-end
-
-r(2)=a(2) - (s(4)*b(2));
-psi(2)=d(2)-(f(4)*b(2))-(s(3)*r(2));
-s(2)=(c(2)-(f(3)*r(2)))/psi(2);
-r(1)=a(1)-(s(3)*b(1));
-psi(1)=d(1)-(f(3)*b(1))-(s(2)*r(1));
-w(2)=(y(2)-(w(4)*b(2))-(w(3)*r(2)))/psi(2);
-w(1)=(y(1) -(w(3)*b(1))- (w(2)*r(1)))/psi(1);
-
-x(1) = w(1);
-x(2) = w(2) -(s(2)*x(1));
-
-% disp("psi:");
-% disp(psi);
-
-for i = 3:1:n
-x(i) = w(i) - (s(i)*x(i-1)) - (f(i)*x(i-2));
-end
-
-% disp("x:");
-% disp(x);
-
-end
-function x = cramer(A,b)
-    d = det(A);
-    x = zeros(size(b));
-    for j = 1:size(b)
-        x(j) = det([A(:,1:j-1) b A(:,j+1:end)]) / d;
-  end
-
+    for i = n-2 : -1 : 3
+        ro(i) = a(i) - sigma(i+2) * b(i);
+        psi(i) = d(i) - fi(i+2) * b(i) - sigma(i+1) * ro(i);
+        sigma(i) = (c(i) - fi(i+1) * ro(i)) / psi(i);
+        fi(i) = e(i) / psi(i);
+        w(i) = (y(i) - w(i+2) * b(i) - w(i+1) *ro(i)) / psi(i);
     end
+
+    ro(2) = a(2) - sigma(4) * b(2);
+    psi(2) = d(2) - fi(4) * b(2) - sigma(3) * ro(2);
+    sigma(2) = (c(2) - fi(3) * ro(2)) / psi(2);
+    ro(1) = a(1) - sigma(3) *b(1);
+    psi(1) = d(1) - fi(3) * b(1) - sigma(2) * ro(1);
+    w(2) = (y(2) - w(4) * b(2) - w(3) * ro(2)) / psi(2);
+    w(1) = (y(1) - w(3) * b(1) - w(2) *ro(1)) / psi(1);
+
+    x(1) = w(1);
+    x(2) = w(2) - sigma(2) * x(1);
+
+    for i = 3 : 1 : n
+        x(i) = w(i) - (sigma(i) * x(i-1)) - fi(i) * x(i-2);
+    end
+
+    disp('psi');
+    disp(psi);
+
+end
+
 
 
 function x = gaussianElimination(A, b)
@@ -208,3 +197,13 @@ end
     
     
 end
+
+function x = cramer(A,b)
+    d = det(A);
+    x = zeros(size(b));
+    for j = 1:size(b)
+        x(j) = det([A(:,1:j-1) b A(:,j+1:end)]) / d;
+  end
+
+    end
+
